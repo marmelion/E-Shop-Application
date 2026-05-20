2348a860fc075b54fec6ed4c21b4f0e23f275e68
Markella Ioannidou <markella.ion12@gmail.com>
2026-05-20 18:06:38 +0300

    Implemented Category Deletion

--- SpringBoot/src/main/java/com/example/demo2/controllers/CategoryController.java
+++ SpringBoot/src/main/java/com/example/demo2/controllers/CategoryController.java
@@ -1,15 +1,15 @@
 package com.example.demo2.controllers;
 
 import com.example.demo2.models.Category;
+import com.example.demo2.models.Product;
 import com.example.demo2.service.CategoryService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -22,5 +22,11 @@ public class CategoryController {
     public ResponseEntity<Category> addCategory(@RequestBody Category category){
         Category newCategory = categoryService.addCategory(category);
         return new ResponseEntity<>(newCategory, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Category> deleteCategory(
+            @RequestParam Long id){
+        return categoryService.deleteCategory(id);
     }
 }
--- SpringBoot/src/main/java/com/example/demo2/service/CategoryService.java
+++ SpringBoot/src/main/java/com/example/demo2/service/CategoryService.java
@@ -1,9 +1,15 @@
 package com.example.demo2.service;
 
 import com.example.demo2.models.Category;
+import com.example.demo2.models.Product;
 import com.example.demo2.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -13,5 +19,14 @@ public class CategoryService {
 
     public Category addCategory(Category category){
         return categoryRepository.save(category);
+    }
+
+    public List<Category> deleteCategory(Long id) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+        if(!categoryOptional.isPresent())
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found");
+        categoryRepository.deleteById(id);
+        return categoryRepository.findAll();
     }
 }
