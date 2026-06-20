929c063fe00fd64602cec58064f51439114f3c7d
Markella Ioannidou <markella.ion12@gmail.com>
2026-06-20 22:47:38 +0300

    Added missing files

---
+++ Front-End/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vinted: '#09b1ba',
+        vintedHover: '#079aa2'
+      }
+    },
+  },
+  plugins: [],
+}
